f3eeef271f7ba433405725a6b8220fe782601b7d
Masato Urai <masato.uraway@gmail.com>
2021-09-05 22:42:41 +0900

    fix format

--- src/components/share-buttons.js
+++ src/components/share-buttons.js
@@ -3,16 +3,16 @@ import { Facebook, Twitter } from 'react-feather';
 import { FacebookShareButton, TwitterShareButton } from 'react-share';
 
 const ShareButtons = ({ url, title, description }) => {
-    return (
-      <div style={{ display: 'flex', gap: '2rem' }}>
-        <FacebookShareButton url={url} quote={title}>
-          <Facebook strokeWidth={1.25} />
-        </FacebookShareButton>
-        <TwitterShareButton url={url} title={title}>
-          <Twitter strokeWidth={1.25} />
-        </TwitterShareButton>
-      </div>
-    );
+  return (
+    <div style={{ display: 'flex', gap: '2rem' }}>
+      <FacebookShareButton url={url} quote={title}>
+        <Facebook strokeWidth={1.25} />
+      </FacebookShareButton>
+      <TwitterShareButton url={url} title={title}>
+        <Twitter strokeWidth={1.25} />
+      </TwitterShareButton>
+    </div>
+  );
 };
 
 export default ShareButtons;
