cfc43fea17fd49b0bef3766cd0bd8c2afc29f4cc
Masato Urai <masato.uraway@gmail.com>
2021-12-12 21:35:51 +0900

    Update gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          'G-2X5XXR86H9', // Google Analytics / GA
+          'G-XPZYWEFYV7', // Google Analytics / GA
           // 'AW-CONVERSION_ID', // Google Ads / Adwords / AW
           // 'DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
